bc42d165e17634a0a94ea139b301e678099efcfa
mai.lephuong <mailephoung@gmail.com>
2024-04-21 22:20:23 +0200

    button add delete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,38 @@
 import React, { useState } from 'react';
-
+import './App.css';
 function App() {
   const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
+  const [showModal, setShowModal] = useState(false);
+  const add = () => {
     setCount(count + 1);
   };
+  const deleteEntry = () => {
+      setShowModal(true);
+    }
+    const confirmDelete = () => {
+        setCount(count - 1);
+        setShowModal(false);
+    };
 
+    const cancelDelete = () => {
+        setShowModal(false);
+    };
   return (
     <div>
       <h1>Hello World</h1>
-      <button onClick={incrementCount}>Click to increment</button>
-      <p>{count}</p> 
+        <p>heyyy</p>
+        <button onClick={add} className="buttonRound" >+</button>
+        <button onClick={deleteEntry} className="buttonRound">-</button>
+        {showModal && (
+            <div className="modal">
+                <div className="modal-content">
+                    <p>Möchten Sie wirklich löschen?</p>
+                    <button onClick={confirmDelete}>Ja</button>
+                    <button onClick={cancelDelete}>Nein</button>
+                </div>
+            </div>
+        )}
+      <p>{count}</p>
     </div>
   );
 }
