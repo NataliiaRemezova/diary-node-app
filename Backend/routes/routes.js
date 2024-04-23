2249030e02cd13b7f2a8a10de89f247d5a11eb50
mai.lephuong <mailephoung@gmail.com>
2024-04-23 20:40:27 +0200

    - Commit untracked files

--- Backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// CRUD operation routes
-router.get('/create-entry', (req, res) => res.send('Create Entry'));
-router.get('/update-entry', (req, res) => res.send('Update Entry'));
-router.get('/delete-entry', (req, res) => res.send('Delete Entry'));
-
-module.exports = router;
