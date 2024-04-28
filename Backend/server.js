8da701702294afa9c00f94d959f0099ac5f0b7a3
Nataliia Remezova <nataliia.remezova@gmail.com>
2024-04-28 17:06:38 +0200

    Backend update: adding model, controller and routes for entry, connecting MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
-const apiRoutes = require('./routes/entryRoutes');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectToDb from './data/connectToDb.js';
+import router from './routes/index.js';
 
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+connectToDb();
+
+// const apiRoutes = require('./routes/entryRoutes');
 
 app.use(express.static('../Frontend/build')); // For the future Vite build
 
-app.use('/api', apiRoutes); // For the future API routes
+app.use('/api', router); 
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
