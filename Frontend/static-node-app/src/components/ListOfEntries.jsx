13735228d88c2c25b8e38d4e7bd71049a44d3005
Matthis <matthis-ehrhardt@gmx.de>
2024-04-28 20:47:02 +0200

    added edit function to entries

--- Frontend/static-node-app/src/components/ListOfEntries.jsx
+++ Frontend/static-node-app/src/components/ListOfEntries.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { MdDeleteForever } from "react-icons/md";
+import { BiSolidEdit } from "react-icons/bi";
 import "../styles/ListOfEntries.css";
 
-function ListOfEntries({ entries, deleteEntry, confirmDelete, cancelDelete, entryToDelete }) {
+function ListOfEntries({ entries, setupEditEntry, deleteEntry, confirmDelete, cancelDelete, entryToDelete }) {
     return (
         <div>
             <h2>List of Entries</h2>
             <ul>
                 {entries.map(entry => (
                     <div key={entry._id} className="entryList">
-                        {entry.text}
+                        {entry.text} {entry.date}
                         <button onClick={() => deleteEntry(entry._id)} className="buttonRound" ><MdDeleteForever /></button>
+                        <button onClick={() => setupEditEntry(entry._id, entry.text)} className="buttonRound" ><BiSolidEdit /></button>
                         {entryToDelete === entry._id && (
                             <div className="modal">
                                 <div className="modal-content">
