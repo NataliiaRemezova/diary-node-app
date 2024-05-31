e45931dca8c26cd8024c6e800a051c9ea7a3353d
Matthis <matthis-ehrhardt@gmx.de>
2024-05-31 14:29:42 +0200

    added user model + controller + router+ model associations + UserTablePage

--- Backend/data/model/habitModel.js
+++ Backend/data/model/habitModel.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const HabitSchema = new mongoose.Schema({
   name: String,
-  completions: [Boolean]
+  completions: [Boolean],
+  user: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
 });
 
 const Habit = mongoose.model('Habit', HabitSchema);
